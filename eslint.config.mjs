a8de77762ff31235250b76cb8a44c157db3067cd
Maksim Koryukov <maxkoryukov@gmail.com>
2026-04-12 06:49:55 -0400

    fix: remove @stylistic/eslint-plugin (in favor of prettier)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,14 @@
 import globals from 'globals';
 import pluginEslintJs from '@eslint/js';
 import pluginMocha from 'eslint-plugin-mocha';
-import pluginStylistic from '@stylistic/eslint-plugin';
 import configEslintConfigPrettier from 'eslint-plugin-prettier/recommended';
 
 export default [
     pluginEslintJs.configs.recommended,
-    pluginStylistic.configs.recommended,
     {
         name: 'whatsapp-web.js/default/rules',
         plugins: {
             mocha: pluginMocha,
-            '@stylistic': pluginStylistic,
         },
         languageOptions: {
             // // TODO: consider moving to:
