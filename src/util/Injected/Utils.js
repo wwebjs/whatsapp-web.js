30a2e33af80fd4abbfae6cf3486dff55d6c20427
tuyuribr <45042245+tuyuribr@users.noreply.github.com>
2026-09-25 11:31:06 -0300

    Update Utils.js

--- src/util/Injected/Utils.js
+++ src/util/Injected/Utils.js
@@ -486,7 +486,7 @@ exports.LoadUtils = () => {
         // MediaData is a model whose private __x_id field collides with Msg's
         // internal id field when its enumerable properties are spread above,
         // breaking getValidatedSender() during Msg initialization.
-        if(message.__x_id){
+        if (message.__x_id) {
             delete message.__x_id;
         }
         // Bot's won't reply if canonicalUrl is set (linking)
